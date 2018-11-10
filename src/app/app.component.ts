d5b507127ec55038e17e68aa2af878d44f4706b7
Carlos Carrero <ccarrero@sotic.com.co>
2018-11-10 10:12:00 -0500

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  ancho: number = 5;
-  largo: number = 6;
+  ancho: number = 4;
+  largo: number = 5;
 
   itemsArray: itemPareja[] = [];
+  itemActual: itemPareja = { idItem: 0, imgBase: "", imgUrl: "", nombreItem: "", abierto: false };
+  imgBase: string = "assets/img/question.png";
 
   constructor(
 
@@ -21,42 +23,70 @@ export class AppComponent {
   }
 
   inicializar() {
-    for (let _i = 0; _i < this.ancho; _i++) {
-      for (let _j = 0; _j < this.largo; _j++) {
-        let nvoItem: itemPareja = {
-          idItem: 1,
-          imgBase: "assets/img/bart.png",
-          imgUrl: "assets/img/abuelo.png",
-          nombreItem: `nombreSticker${_i}-${_j}`,
-        }
-        this.itemsArray.push(nvoItem);
-      }
-    }
+    this.arreglo();
     this.shuffle(this.itemsArray);
   }
 
   clickItem(item: itemPareja) {
-    console.log(item.nombreItem);
     item.imgBase = item.imgUrl;
+    item.abierto = true;
+    if (this.itemActual.idItem == 0) {
+      this.itemActual = {
+        idItem: item.idItem, imgBase: item.imgBase, imgUrl: item.imgUrl, nombreItem: item.nombreItem, abierto: false
+      };
+    } else {
+      if (this.itemActual.idItem == item.idItem) {
+        console.log('ok');
+      } else {
+        console.log('no');
+        setTimeout(() => {
+          this.itemsArray.forEach(function (element) {
+            if (element.abierto) {
+              element.imgBase = "assets/img/question.png";
+              element.abierto = false;
+            }
+          });
+        }, 1000);
+      }
+      this.itemActual = { idItem: 0, imgBase: "", imgUrl: "", nombreItem: "", abierto: false };
+    }
   }
 
   shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-
-      // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
 
-      // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
     }
 
     return array;
+  }
+
+  arreglo() {
+    this.itemsArray.push({ idItem: 1, imgBase: this.imgBase, imgUrl: "assets/img/abuelo.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 1, imgBase: this.imgBase, imgUrl: "assets/img/abuelo.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 2, imgBase: this.imgBase, imgUrl: "assets/img/apu.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 2, imgBase: this.imgBase, imgUrl: "assets/img/apu.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 3, imgBase: this.imgBase, imgUrl: "assets/img/bart.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 3, imgBase: this.imgBase, imgUrl: "assets/img/bart.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 4, imgBase: this.imgBase, imgUrl: "assets/img/flanders.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 4, imgBase: this.imgBase, imgUrl: "assets/img/flanders.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 5, imgBase: this.imgBase, imgUrl: "assets/img/homero.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 5, imgBase: this.imgBase, imgUrl: "assets/img/homero.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 6, imgBase: this.imgBase, imgUrl: "assets/img/liza.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 6, imgBase: this.imgBase, imgUrl: "assets/img/liza.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 7, imgBase: this.imgBase, imgUrl: "assets/img/maggie.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 7, imgBase: this.imgBase, imgUrl: "assets/img/maggie.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 8, imgBase: this.imgBase, imgUrl: "assets/img/marge.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 8, imgBase: this.imgBase, imgUrl: "assets/img/marge.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 9, imgBase: this.imgBase, imgUrl: "assets/img/milgouse.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 9, imgBase: this.imgBase, imgUrl: "assets/img/milgouse.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 10, imgBase: this.imgBase, imgUrl: "assets/img/skinner.png", nombreItem: "", abierto: false });
+    this.itemsArray.push({ idItem: 10, imgBase: this.imgBase, imgUrl: "assets/img/skinner.png", nombreItem: "", abierto: false });
   }
 }
 
@@ -65,4 +95,5 @@ interface itemPareja {
   imgUrl: string;
   imgBase: string;
   nombreItem: string;
+  abierto: boolean;
 }
